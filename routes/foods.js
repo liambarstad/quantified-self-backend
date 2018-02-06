a13a156457540fd888b85d36495008cdfa73ce95
Liam Barstad <liambarstad@gmail.com>
2018-02-06 10:33:03 -0700

    wip

--- routes/foods.js
+++ routes/foods.js
@@ -11,6 +11,21 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/', function(req, res, next) {
+  let name = req.body.food.name;
+  let calories = req.body.food.calories;
+  if (name && calories) {
+    database.raw('INSERT INTO foods (name, calories, created_at) VALUES(?, ?, ?) RETURNING *', [name, calories, new Date])
+      .then(function(created) {
+        if (created) {
+          return res.status(200).json(created);
+        } else {
+          return res.status(400);
+        }
+      });
+  };
+});
+
 router.get('/:id', function(req, res, next) {
   let id = req.params.id
   database.raw('SELECT * FROM foods WHERE id=?', id)
@@ -24,18 +39,39 @@ router.get('/:id', function(req, res, next) {
     });
 });
 
-router.post('/', function(req, res, next) {
-  let name = req.body['food']['name'];
-  let calories = req.body['food']['calories'];
+router.patch('/:id', function(req, res, next) {
+  let id = req.params.id;
+  let name = req.body.food.name;
+  let calories = req.body.food.calories;
   if (name && calories) {
-    database.raw('INSERT INTO foods (name, calories, created_at) VALUES(?, ?, ?) RETURNING *', [name, calories, new Date])
-      .then(function(created) {
-        if (created) {
-          return res.status(200).json(created);
-        } else {
-          return res.status(400);
-        }
-      });
-  };
+    let changed_value = `name=${name}, calories=${calories}`;
+  } else if (name) {
+    let chaged_value = `name=${name}`;
+  } else if (calories) {
+    let changed_value = `calories=${calories}`;
+  } else {
+    return res.status(400);
+  }
+  database.raw('UPDATE foods SET ' + changed_value + ' WHERE id=? RETURNING *', id)
+    .then(function(changed) {
+      if (changed) {
+        return res.status(200).json(changed);
+      } else {
+        return res.status(400);
+      }
+    });
 });
+
+router.delete('/:id', function(req, res, next) {
+  let id = req.params.id;
+  database.raw('DELETE FROM foods WHERE id=? RETURNING *', id)
+    .then(function(deleted) {
+      if (deleted) {
+        return res.status(200).json(deleted);
+      } else {
+        return res.status(404);
+      }
+    });
+});
+
 module.exports = router;
