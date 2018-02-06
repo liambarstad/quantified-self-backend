f41ebdbfdc704b610776f597fe8ba777a86f3743
Liam Barstad <liambarstad@gmail.com>
2018-02-06 00:12:19 -0700

    add single food lookup

--- routes/foods.js
+++ routes/foods.js
@@ -6,9 +6,21 @@ const database = require('knex')(configuration)
 
 router.get('/', function(req, res, next) {
   database.raw('SELECT * FROM foods')
-    .then(function(foods) {
-      return res.status(200).json(foods['rows']);
+    .then(function(result) {
+      return res.status(200).json(result['rows']);
     });
 });
 
+router.get('/:id', function(req, res, next) {
+  let id = req.params.id
+  database.raw('SELECT * FROM foods WHERE id=?', id)
+    .then(function(result) {
+      let food = result['rows'][0]
+      if (food) {
+        return res.status(200).json(food);
+      } else {
+        return res.status(404);
+      }
+    });
+});
 module.exports = router;
