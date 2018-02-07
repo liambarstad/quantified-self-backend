a5a924729f24d15faed5a54a342a253b7ba18a5d
Liam Barstad <liambarstad@gmail.com>
2018-02-06 22:05:23 -0700

    tweak beforeEach

---
+++ test/foods_model_test.spec.js
@@ -0,0 +1,53 @@
+const pry = require('pryjs')
+const chai = require('chai')
+const should = chai.should()
+const Foods = require('../models/foods')
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+describe('Foods Model Tests', () => {
+  before(done => {
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error
+      })
+  })
+
+  beforeEach((done) => {
+    //database('foods').truncate()
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error
+      })
+  })
+
+  it('can get all foods', () => {
+    Foods.getAll()
+      .then(foods => {
+        foods.should.be.a('array')
+        foods.length.should.equal(13)
+        foods[0].should.have.property('id')
+        foods[0].should.have.property('name')
+        foods[0].should.have.property('calories')
+      })
+  })
+
+  it('can get a single food', () => {
+
+  })
+
+  it('can create a new food', () => {
+
+  })
+
+  it('can update a food', () => {
+
+  })
+
+  it('can delete a food', () => {
+
+  })
+})
