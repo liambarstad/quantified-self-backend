65ebc607a0f067856cf5a8d17772d899e6951bbf
Miguel Silva <msilva0897@gmail.com>
2018-02-07 10:21:13 -0700

    meals done

--- routes/meals.js
+++ routes/meals.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('../knexfile')[environment]
-const database = require('knex')(configuration)
+const Meals = require('../models/meals')
 
-router.get('/api/v1/meals', (request, response) => {
+router.get('/', (req, res) => {
+  Meals.allMeals()
+  .then((data) => {
+    if (data.rowCount == 0) {
+      return res.sendStatus(404)
+    };
+    data.rows.forEach((element) => {
+        if (element.foods[0].id === null) {
+          element.foods = [];
+        }
+    });
+    res.json(data.rows)
+  });
+})
 
+router.get('/:meal_id/foods', (req, res) => {
+  const meal_id = req.params.meal_id
+  Meals.mealsFoods(meal_id)
+  .then((data) => {
+    if (data.rowCount === 0) {
+      return res.sendStatus(404)
+    }
+    res.status(201).json(data.rows);
+  });
+});
+
+
+router.post('/:meal_id/foods/:id', (req, res) => {
+  const meal_id = req.params.meal_id
+  const food_id = req.params.id
+  Meals.addFood(meal_id, food_id)
+  .then((data)=>{
+    if (data.rowCount === 0) {
+      return res.sendStatus(404)
+    }
+    res.status(201).send("Successfully added food to meal")
+  })
+})
+
+router.delete('/:meal_id/foods/:id', (req, res) => {
+  const meal_id = req.params.meal_id
+  const food_id = req.params.id
+  Meals.deleteFood(meal_id, food_id)
+  .then((data)=>{
+    if (data.rowCount === 0) {
+      return res.sendStatus(404)
+    }
+    res.status(201).send("Successfully Deleted Food From Meal")
+  })
 })
 
 
 module.exports = router;
-//Select meals.id, meals.name From (SELECT foods.id, foods.name, foods.calories) AS foods From meals Left Outer Join meal_foods ON meal_foods.meal = meals.id Left Outer Join foods On food.id = meal_foods.food Group By meals.id; 
